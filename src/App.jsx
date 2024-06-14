1aa77cffa60bdd62e5ae33f5c47106de173ba7c0
gurdiny <gerardour619@gmail.com>
2024-06-13 19:33:16 -0600

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import NavBar from "./components/NavBar";
+export default function App() {
+  return (
+    <main className="w-full min-h-screen p-4 grid grid-cols[20%_1fr] grid-rows-[3.2rem_1fr]">
+      <NavBar />
+    </main>
+  );
+}
