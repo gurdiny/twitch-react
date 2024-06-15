84ddad35782ee8304fec2cb0ff9cc97dcaafbbe1
gurdiny <gerardour619@gmail.com>
2024-06-14 20:50:40 -0600

    Add Nuevo Tag

--- src/components/loops/ChannelsCategories.jsx
+++ src/components/loops/ChannelsCategories.jsx
@@ -1,7 +1,12 @@
 export default function ChannelsCategories({ title, viewers, tags, image }) {
   return (
     <div className="flex flex-col hover:overflow-hidden">
-      <div className="lg:bg-[#a970ff] hover:overflow-hidden group-hover:shadow-lg">
+      <div className="lg:bg-[#a970ff] hover:overflow-hidden group-hover:shadow-lg relative">
+        {tags.includes("IRL") && (
+          <div className="absolute text-black text-[10px] z-20 top-4 right-4 bg-[#FF75E6] rounded-xl pl-2 pr-2 font-semibold">
+            <span>NUEVO</span>
+          </div>
+        )}
         <img
           className="hover:ml-2 hover:mb-2 hover:overflow-hidden transition-transform duration-300 transform group-hover:scale-105"
           src={image}
